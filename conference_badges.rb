388b23c770a36384608ef86ae5b5959b9a917649
Kelvyn King <kelvyn.m.king@gmail.com>
2022-06-24 15:27:23 +1000

    Created required methods. Completed lab.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    results = []
+    names.map{|name| results << "Hello, my name is #{name}."}
+    results
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) {|name, room_number| "Hello, #{name}! You'll be assigned to room #{room_number}!"}
+end
+
+def printer(names)
+   batch_badge_creator(names).each {|badge| puts badge}
+   assign_rooms(names).each {|room| puts room}
+end
